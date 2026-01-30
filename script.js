bf675cebb59bff0cdd5d1642ef0e0f0078c4f995
TakashiNishii <ygortn14@gmail.com>
2026-01-30 19:01:12 -0400

    Adiciona animações de entrada e animações de rolagem para cards e seção de agradecimento

--- script.js
+++ script.js
@@ -6,3 +6,47 @@ ScrollSmoother.create({
   effects: true
 })
 
+// Animações de entrada
+gsap.from(".hero", {
+  opacity: 0,
+  duration: 1
+})
+
+gsap.from("picture:nth-child(2)", {
+  y: 60,
+  duration: 1,
+})
+
+gsap.from("picture:nth-child(1)", {
+  y: -60,
+  duration: 1,
+})
+
+
+// Animações Card
+// ScrollTrigger serve para ativar animações com a rolagem
+gsap.from(".card", {
+  opacity: 0,
+  filter: "blur(10px)",
+  stagger: .3,
+  scrollTrigger: {
+    trigger: ".cards",
+    // markers: true, O "markers: true" serve para mostrar os marcadores de início e fim do ScrollTrigger
+    start: "0% 80%", // quando 80% do viewport alcançar 0% do gatilho
+    end: "100% 70%", // quando 70% do viewport alcançar 100% do gatilho
+    scrub: true, // animação vinculada à rolagem e adicionar um numero inves de true faz a animação mais lenta no maximo colocar 3
+  }
+})
+
+gsap.from(".secaoObrigado ul li", {
+  opacity: 0,
+  x: 40,
+  filter: "blur(10px)",
+  stagger: .1,
+  scrollTrigger: {
+    trigger: ".secaoObrigado ul",
+    start: "0% 80%",
+    end: "100% 50%",
+    scrub: true,
+  }
+})
